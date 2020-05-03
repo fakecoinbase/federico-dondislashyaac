ad98ad10848c5af87366aeb725f1d4723b83ede0
Federico Dondi <federico.dondi2@studio.unibo.it>
2020-05-03 22:31:37 +0200

    Added basic Transaction

---
+++ src/Transaction.ts
@@ -0,0 +1,35 @@
+import {
+  createHash,
+  createSign,
+  createVerify
+} from 'crypto'
+
+import Wallet from './Wallet'
+
+export default class Transaction {
+  readonly signature: string
+
+  constructor (
+    readonly amount: number,
+    readonly fromAddress: string,
+    readonly toAddress: string,
+    readonly nonce: number,
+    issuer: Wallet
+  ) {
+    this.signature = issuer.sign(this.hash)
+  }
+
+  readonly timestamp: number = Date.now()
+
+  public get hash (): string {
+    const json = JSON.stringify(
+      this.amount +
+      this.fromAddress +
+      this.toAddress +
+      this.timestamp +
+      this.nonce
+    )
+
+    return createHash('SHA256').update(json).digest('hex')
+  }
+}
